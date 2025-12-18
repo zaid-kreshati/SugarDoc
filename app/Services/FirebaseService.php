8a6ff8aa86c3de9636fb0fd6747cf5f161ea4662
zaid <112625885+zaid-kreshati@users.noreply.github.com>
2025-12-18 09:00:56 +0300

    3

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -1,48 +1,116 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Notifications;
 
+
+use Exception;
 use Google\Client as GoogleClient;
 use Illuminate\Support\Facades\Http;
-
-class FirebaseService
+use Illuminate\Support\Facades\Log;
+class FirebaseNotificationService
 {
-    protected function getAccessToken(): string
+    protected $projectId;
+    protected $credentials;
+
+    public function __construct()
     {
-
-    //     $firebase = (new \Kreait\Firebase\Factory)
-    // ->withServiceAccount(config('app.firebase_credentials'))
-    // ->create();
-
-
-        $client = new GoogleClient();
-        $client->setAuthConfig('/etc/secrets/firebase_credentials.json');
-
-
-        // $client->setAuthConfig(storage_path('app/firebase/firebase_credentials.json'));
-        $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
-
-        $token = $client->fetchAccessTokenWithAssertion();
-
-        return $token['access_token'];
+        $this->credentials = config('firebase.credentials');
     }
 
-    public function sendNotification(string $firebaseToken, string $title, string $body): void
+    /**
+     * @throws \Google\Exception
+     * @throws \Illuminate\Http\Client\ConnectionException
+     */
+    public function sendNotification(string $token, string $title, string $body): array
     {
-        $accessToken = $this->getAccessToken();
+        $client = new GoogleClient();
+        $client->setAuthConfig($this->credentials);
+        $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+        $client->refreshTokenWithAssertion();
 
-        Http::withToken($accessToken)
-            ->post(
-                'https://fcm.googleapis.com/v1/projects/' . config('services.firebase.project_id') . '/messages:send',
-                [
-                    'message' => [
-                        'token' => $firebaseToken,
+        $accessToken = $client->getAccessToken()['access_token'];
+
+
+        $response = Http::withToken($accessToken)
+            ->withHeaders(['Content-Type' => 'application/json'])
+            ->post("https://fcm.googleapis.com/v1/projects/{$this->projectId}/messages:send", [
+                'message' => [
+                    'token' => $token,
+                    'notification' => [
+                        'title' => $title,
+                        'body' => $body,
+                    ],
+                    'android' => [
+                        'priority' => 'high',
                         'notification' => [
-                            'title' => $title,
-                            'body' => $body,
+                            'sound' => 'default', // Or use a custom sound uploaded to the app
                         ],
                     ],
+                    'data' => [
+                        'type' => 'chat_message',
+                        'sender_id' => '123',
+                        'conversation_id' => '456',
+                        'click_action' => 'FLUTTER_NOTIFICATION_CLICK', // Required for background tap to work
+                    ]
                 ]
-            );
+            ]);
+
+        if ($response->failed()) {
+
+            Log::info("Erroreeeee" . $response->body());
+            throw new Exception($response->body());
+        }
+
+
+        return $response->json();
+    }
+
+
+    /**
+     * @throws \Google\Exception
+     * @throws \Illuminate\Http\Client\ConnectionException
+     */
+    public function sendNotificationNew(string $token, string $title, string $body): array
+    {
+        $client = new GoogleClient();
+        $client->setAuthConfig($this->credentials);
+        $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+        $client->refreshTokenWithAssertion();
+
+        $accessToken = $client->getAccessToken()['access_token'];
+
+        $message = [
+            'message' => [
+                'token' => $token,
+                'notification' => [
+                    'title' => $title,
+                    'body' => $body,
+                ],
+                'android' => [
+                    'priority' => 'high',
+                    'notification' => [
+                        'sound' => 'default', // Or use a custom sound uploaded to the app
+                    ],
+                ],
+                'data' => [
+                    'type' => 'chat_message',
+                    'sender_id' => '123',
+                    'conversation_id' => '456',
+                    'click_action' => 'FLUTTER_NOTIFICATION_CLICK', // Required for background tap to work
+                ],
+
+            ]
+        ];
+
+        $response = Http::withToken($accessToken)
+            ->withHeaders(['Content-Type' => 'application/json'])
+            ->post("https://fcm.googleapis.com/v1/projects/{$this->projectId}/messages:send", $message);
+
+        if ($response->failed()) {
+            Log::info("Erroreeeee" . $response->body());
+            throw new Exception($response->body());
+        }
+
+        return $response->json();
     }
 }
---
+++ config/firebase.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'credentials' => storage_path('app/firebase/firebase_credentials.json'),
+];
